03e05f7f814397e131f6743eca946a0ee0e18d03
frappe <saranyaswaminathan89@gmail.com>
2025-10-10 17:34:10 +0530

    Refactored

---
+++ hatim_carbon/patches/batch_enable.py
@@ -0,0 +1,47 @@
+import frappe
+
+def execute():
+
+    excluded_groups = [
+    "Adhesives & Resins",
+    "Dies & Tooling",
+    "Electrical Items",
+    "Machined FG Products",
+    "Machinery Tools",
+    "Oils & Lubricants",
+    "Terminal",
+    "Tubes & Rods",
+    "Sleeving",
+    "Packaging Material",
+    "Fasteners"
+]
+
+
+    items = frappe.get_all(
+        "Item",
+        filters=[
+            ["disabled", "=", 0],
+            ["is_stock_item", "=", 1],           
+            ["has_batch_no", "=", 0],           
+            ["item_group", "not in", excluded_groups]
+        ],
+        fields=["name", "item_code", "item_name"]
+    )
+
+    print(f"Found {len(items)} items to update batch.")
+
+    for i, item in enumerate(items, start=1):
+
+        item_doc = frappe.get_doc("Item", item.name)        
+
+        frappe.db.set_value("Item", item.name, {
+            "allow_negative_stock": 1,
+            "has_batch_no": 1,
+            "create_new_batch": 1
+        })
+
+        frappe.db.commit()
+        print(f"Batch enabled for Item: {item_doc.name}\n")
+
+
+   
---
+++ hatim_carbon/patches/stock_enabe.py
@@ -0,0 +1,28 @@
+import frappe
+
+def execute():
+    # Filter for non-stock items
+    items = frappe.get_all(
+        "Item",
+        filters={
+            "is_stock_item": 0,
+            "has_batch_no": 0,
+            "disabled": 0
+        },
+        fields=["name", "item_code", "item_name"]
+    )
+
+    print(f"Found {len(items)} items to update stock.")
+
+    for i, item in enumerate(items, start=1):
+        item_doc = frappe.get_doc("Item", item.name)        
+
+        frappe.db.set_value("Item", item.name, {            
+            "is_stock_item": 1,
+            "allow_negative_stock": 1
+        })
+
+        frappe.db.commit()
+        print(f"Stock enabled for Item: {item_doc.name}\n")        
+
+        
