798305c9906b59d46c5b618bec46aab0fda12290
kponsakthivel <kponsakthivel@gmail.com>
2025-12-15 11:02:01 +0530

    job card start button enable

--- hatim_carbon/hooks.py
+++ hatim_carbon/hooks.py
@@ -43,7 +43,6 @@ app_license = "mit"
 # page_js = {"page" : "public/js/file.js"}
 
 # include js in doctype views
-doctype_js = {"Job Card" : "public/js/job_card.js"}
 
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
